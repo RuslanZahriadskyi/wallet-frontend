a7f520f55d71e878d02c07557fcc97ba0787f906
Ruslan Zahriadskyi <ruslan.zahriadskyi@gmail.com>
2021-07-04 10:26:27 +0200

    add colors and private,public router

---
+++ src/components/PrivateRouter.js
@@ -0,0 +1,10 @@
+// import React from 'react';
+// import { Redirect, Route } from 'react-router';
+
+// export default function PrivateRouter({ children, ...routeProps }) {
+//   return (
+//     <Route {...routeProps}>
+//       {????? ? children : <Redirect to="/login" />}
+//     </Route>
+//   );
+// }
---
+++ src/components/PublicRouter.js
@@ -0,0 +1,14 @@
+// import React from 'react';
+// import { Redirect, Route } from 'react-router';
+
+// export default function PublicRouter({ children, ...routeProps }) {
+//   return (
+//     <Route {...routeProps}>
+//       {?? && routeProps.restricted ? (
+//         <Redirect to="???" />
+//       ) : (
+//         children
+//       )}
+//     </Route>
+//   );
+// }
