a2094ca974894895524548b0dfd01ee725f3f3ee
Tatyana Loza <jes220@ukr.net>
2021-07-08 19:47:16 +0300

    validation

--- src/components/RegistrationForm/RegistrationForm.jsx
+++ src/components/RegistrationForm/RegistrationForm.jsx
@@ -1,82 +1,62 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { NavLink } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import { authOperations } from '../../redux/auth';
 import * as yup from 'yup';
-import { Formik } from 'formik';
-
-const schema = yup.object({
-  username: yup.string().required('Please Enter a username'),
-  email: yup.string().email().required('Please Enter your Email'),
-  confirmEmail: yup
-    .string()
-    .email()
-    .required()
-    .oneOf([yup.ref('email'), null], 'Emails must match'),
-  password: yup
-    .string()
-    .min(6, 'Password must be at least 6 characters')
-    .max(12, 'Password must be up to 12 characters')
-    .required('Please Enter your password'),
-
-  confirmPassword: yup
-    .string()
-    .required()
-    .oneOf([yup.ref('password'), null], 'Passwords must match'),
-});
+import { useFormik } from 'formik';
 
 export default function RegistrationForm() {
   const dispatch = useDispatch();
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  // const [repeat, setRepeat] = useState("");
-  const [name, setName] = useState('');
 
-  const handleChangeEmail = e => {
-    setEmail(e.target.value);
-  };
+  const formik = useFormik({
+    initialValues: {
+      email: '',
+      password: '',
+      confirmPassword: '',
+      name: '',
+    },
 
-  const handleChangePassword = e => {
-    setPassword(e.target.value);
-  };
+    validation: yup.object({
+      username: yup.string().required('Please Enter a username'),
+      email: yup.string().email().required('Please Enter your Email'),
+      confirmEmail: yup
+        .string('Enter your email')
+        .email('Enter a valid email')
+        .required('Email is required')
+        .oneOf([yup.ref('email'), null], 'Emails must match'),
+      password: yup
+        .string('Enter your password')
+        .min(6, 'Password must be at least 6 characters')
+        .max(12, 'Password must be up to 12 characters')
+        .required('Please enter your password'),
 
-  //  const handleChangeRepeat = e => {
-  //   setRepeat(e.target.value)
-  // }
+      confirmPassword: yup
+        .string('Enter your password')
+        .required('Please enter your password')
+        .oneOf([yup.ref('password'), null], 'Passwords must match'),
 
-  const handleChangeName = e => {
-    setName(e.target.value);
-  };
+      name: yup
+        .string('Enter your name')
+        .min(3, 'Password must be at least 3 characters')
+        .max(12, 'Password must be up to 12 characters')
+        .required('Please Enter your name'),
+    }),
 
-  const handleSubmit = e => {
-    e.preventDefault();
-
-    dispatch(authOperations.register({ name, email, password }));
-
-    setEmail('');
-    setPassword('');
-    setName('');
-  };
+    onSubmit: ({ email, password, name }) => {
+      dispatch(authOperations.register({ name, email, password }));
+    },
+  });
 
   return (
     <div>
-      <Formik
-        validationSchema={schema}
-        onSubmit={handleSubmit}
-        initialValues={{
-          email: '',
-          password: '',
-          name: '',
-        }}
-      ></Formik>
       {/* <Title text='Wallet' /> */}
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={formik.handleSubmit}>
         <label>
           <input
             type="email"
             name="email"
-            onChange={handleChangeEmail}
-            value={email}
+            onChange={formik.handleChange}
+            value={formik.email}
             placeholder="E-mail"
             required
           />
@@ -85,28 +65,28 @@ export default function RegistrationForm() {
           <input
             type="password"
             name="password"
-            onChange={handleChangePassword}
-            value={password}
+            onChange={formik.handleChange}
+            value={formik.password}
             placeholder="Пароль"
             required
           />
         </label>
-        {/* <label>
-        <input
-          type="password"
-          name="repeat"
-          onChange={handleChangePassword}
-          value={repeat}
-          placeholder="Подтвердите пароль"
-          required
-        />
-      </label> */}
+        <label>
+          <input
+            type="password"
+            name="confirmPassword"
+            onChange={formik.handleChange}
+            value={formik.confirmPassword}
+            placeholder="Подтвердите пароль"
+            required
+          />
+        </label>
         <label>
           <input
             type="name"
             name="name"
-            onChange={handleChangeName}
-            value={name}
+            onChange={formik.handleChange}
+            value={formik.name}
             placeholder="Ваше имя"
             required
           />
