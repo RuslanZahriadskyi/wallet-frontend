28ff0923726f0bbd6a8eb81b9251c0cd27b46d77
Tatyana Loza <jes220@ukr.net>
2021-07-11 23:57:52 +0300

    new register

--- src/views/RegistrationPage.jsx
+++ src/views/RegistrationPage.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import RegistrationForm from '../components/RegistrationForm/RegistrationForm';
+import images from '../images/login-desktop.png';
+import './RegistrationPage';
 
 const RegistrationPage = () => {
-  return <div>
-     <RegistrationForm />
-  </div>;
+  return (
+    <div className="registrationPage">
+      <div>
+        <img src={images} alt="RegPage" className="images" />
+      </div>
+      <RegistrationForm />
+    </div>
+  );
 };
 
 export default RegistrationPage;
