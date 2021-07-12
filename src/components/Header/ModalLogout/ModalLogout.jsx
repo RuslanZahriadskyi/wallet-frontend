12e0b4905e8be236966704cd8b7ff84d79cf766b
Nika <64929516+nikule4ka@users.noreply.github.com>
2021-07-12 18:41:59 +0300

    resolve problem with buttons

--- src/components/Header/ModalLogout/ModalLogout.jsx
+++ src/components/Header/ModalLogout/ModalLogout.jsx
@@ -1,12 +1,22 @@
 import React from 'react';
+import { useCallback } from 'react';
+import { useDispatch } from 'react-redux';
+import { operationsAction } from '../../../redux/operations';
+
 import './ModalLogout.scss';
 import FormButtons from '../../FormButtons';
 
 const ModalLogout = () => {
+  const dispatch = useDispatch();
+  const logoutModalAction = useCallback(
+    () => dispatch(operationsAction.logoutModalAction()),
+    [dispatch],
+  );
   return (
     <div className="container_modal_logout">
       <h2 className="title_modal_logout">Вы уверены, что хотите выйти?</h2>
       <FormButtons
+        canselAction={logoutModalAction}
         className="btn_logout_exit"
         firtsButtonText="ВЫЙТИ"
         secondButtonText="ОТМЕНА"
