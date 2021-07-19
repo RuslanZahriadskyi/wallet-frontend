10bf037261b36756b820b5b1682f654621361fb9
Nika <64929516+nikule4ka@users.noreply.github.com>
2021-07-19 22:19:27 +0300

    edit ellipses

--- src/components/MainContainer/DesktopMainContainer/DesktopMainContainer.jsx
+++ src/components/MainContainer/DesktopMainContainer/DesktopMainContainer.jsx
@@ -3,20 +3,24 @@ import React from 'react';
 import HomeDesktop from '../../Home/HomeDesktop';
 import TransactionDesktop from '../../Transaction/TransactionDesktop';
 import TransactionMobile from '../../Transaction/TransactionMobile';
-import CurrencyTab from '../../Currency/CurrencyTab';
+import CurrencyTab from '../../Currency';
 
 import './DesktopMainContainer.scss';
 
 const DesktopMainContainer = () => {
   return (
     <div className="dashboard-container">
-      <div className="dashboard-top-container">
-        <HomeDesktop />
-        <CurrencyTab />
-      </div>
+      <div className="background-blur">
+        <div className="bg_filter">
+          <div className="dashboard-top-container">
+            <HomeDesktop />
+            <CurrencyTab />
+          </div>
 
-      <TransactionDesktop />
-      {/* <TransactionMobile /> */}
+          <TransactionDesktop />
+          {/* <TransactionMobile /> */}
+        </div>
+      </div>
     </div>
   );
 };
